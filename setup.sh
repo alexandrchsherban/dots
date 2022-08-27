f9d4d32dd13e4f33226a681b3b20a86cfff2a4c2
alexandrchsherban <alexandrchsherban>
2022-08-27 22:07:23 +0500

    xsel and xclip added

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ gsettings set org.gnome.shell.keybindings switch-to-application-9 []
 gsettings set org.gnome.shell.window-switcher current-workspace-only false
 sudo apt update 
 sudo apt upgrade -y 
-sudo apt install apt-transport-https curl wget gnome-tweaks neofetch git python3-pip python3-venv tmux libglib2.0-dev -y
+sudo apt install apt-transport-https curl wget gnome-tweaks neofetch git python3-pip python3-venv tmux libglib2.0-dev xclip xsel -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
